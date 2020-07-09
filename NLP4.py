a43b02d3dbe1243fc25266ed5e50912a975aece0
ccw_530 <chanchiwing530@gmail.com>
2020-07-09 13:32:14 +0800

    NLP4

--- NLP4.py
+++ NLP4.py
@@ -59,33 +59,35 @@ def get_top_news():
 
 with open("headlines_top_news.pickle", "rb") as f:
     headlines = pickle.load(f)
-def change_dates_tocsv():
+def change_dates_tocsv(headlines, today='', yesterday=''):
     df1 = pd.DataFrame(headlines, columns=('Headlines', 'Dates', 'Ticker'))
     df1.iloc[:, 0].replace(['^a-zA-Z'], ' ', regex=True, inplace=True)
     df1 = df1.drop_duplicates()
     df1 = df1.sort_values(by='Dates')
     df1.reset_index(drop=True, inplace=True)
     for i in range(len(df1['Dates'])):
-        match = re.search(r'\w{3}\.\s\d{1,2}\,\s\d{4}|May\s\d{1,2}\,\s\d{4}|\w{3}\.\s\d{1,2}|May\s\d{1,2}|\bToday\b',
-                          df1['Dates'][i])
+        match = re.search(r'\w{3}\.\s\d{1,2}\,\s\d{4}|May\s\d{1,2}\,\s\d{4}|\w{3}\.\s\d{1,2}|May\s\d{1,2}|\bToday\b|\bYesterday\b',
+                          df1['Dates'].iloc[i])
         if re.search(r'\w{3}\.\s\d{1,2}\,\s\d{4}|\w{3}\s\d{1,2}\,\s\d{4}', match[0]):
             fulldate = match[0]
         elif re.search(r'\bToday\b', match[0]):
-            fulldate = 'Jun 22, 2020'
+            fulldate = today
+        elif re.search(r'\bYesterday\b', match[0]):
+            fulldate = yesterday
         else:
             fulldate = match[0] + ", 2020"
 
-        if len(df1['Dates'][i].split(',')) == 2:
-            time = datetime.datetime.strptime(df1['Dates'][i].split(',')[1].strip(), '%I:%M %p').time()
-        elif len(df1['Dates'][i].split(',')) == 3:
-            time = datetime.datetime.strptime(df1['Dates'][i].split(',')[2].strip(), '%I:%M %p').time()
+        if len(df1['Dates'].iloc[i].split(',')) == 2:
+            time = datetime.datetime.strptime(df1['Dates'].iloc[i].split(',')[1].strip(), '%I:%M %p').time()
+        elif len(df1['Dates'].iloc[i].split(',')) == 3:
+            time = datetime.datetime.strptime(df1['Dates'].iloc[i].split(',')[2].strip(), '%I:%M %p').time()
 
         if time > datetime.time(16):
             for fmt in ('%b. %d, %Y', '%b %d, %Y'):
                 try:
                     newDate = datetime.datetime.strptime(fulldate, fmt).date()
                     newDate = newDate + datetime.timedelta(days=1)
-                    df1['Dates'][i] = newDate
+                    df1['Dates'].iloc[i] = newDate
                     break
                 except ValueError:
                     pass
@@ -93,12 +95,12 @@ def change_dates_tocsv():
             for fmt in ('%b. %d, %Y', '%b %d, %Y'):
                 try:
                     newDate = datetime.datetime.strptime(fulldate, fmt).date()
-                    df1['Dates'][i] = newDate
+                    df1['Dates'].iloc[i] = newDate
                     break
                 except ValueError:
                     pass
     df1.to_csv('Top_News_2.csv', index=False)
-# change_dates_tocsv()
+change_dates_tocsv(headlines, today='Jul. 8, 2020', yesterday='Jul. 7, 2020')
 
 stop_words = set(stopwords.words('english'))
 ps = PorterStemmer()
@@ -106,9 +108,7 @@ ps = PorterStemmer()
 df1 = pd.read_csv('Top_News_2.csv')
 df1.iloc[:, 0].replace(['^a-zA-Z'], ' ', regex=True, inplace=True)
 
-def create_price():
-    start = datetime.datetime(2015, 12, 20)
-    end = datetime.datetime(2020, 6, 22)
+def create_price(start, end):
     df_stock = web.DataReader('^GSPC', 'yahoo', start, end)
     df_stock.to_csv('^GSPC_2.csv')
     df2 = pd.read_csv('^GSPC_2.csv', index_col=0)
@@ -116,28 +116,30 @@ def create_price():
     df2['Return'] = df2['Adj Close'] / df2['Adj Close'].shift(1) - 1
     df2['Label'] = ''
     for i in range(len(df2['Return'])):
-        if df2['Return'][i] >= 0:
-            df2['Label'][i] = 0
-        elif df2['Return'][i] < 0:
-            df2['Label'][i] = 1
+        if df2['Return'].iloc[i] >= 0:
+            df2['Label'].iloc[i] = 0
+        elif df2['Return'].iloc[i] < 0:
+            df2['Label'].iloc[i] = 1
         else:
-            df2['Label'][i] = np.nan
+            df2['Label'].iloc[i] = np.nan
     return df2
-# df2 = create_price()
+start = datetime.datetime(2015, 12, 20)
+end = datetime.datetime(2020, 6, 22)
+df2 = create_price(start, end)
 
-def stopwords_and_stemmimg(df):
+def stopwords_and_stemmimg(df, df2):
     date_dict = {}
     data_for_df = []
     for i in range(len(df['Dates'])):
-        date_dict[df['Dates'][i]] = []
+        date_dict[df['Dates'].iloc[i]] = []
 
     for i in range(len(df['Headlines'])):
-        news = df['Headlines'][i]
+        news = df['Headlines'].iloc[i]
         word_tokens = nltk.word_tokenize(news)
         filtered_sentence = [w for w in word_tokens if w not in stop_words]
         stemmed_sentence = [ps.stem(w) for w in filtered_sentence]
         news = ' '.join(stemmed_sentence)
-        date_dict[df['Dates'][i]].append(news)
+        date_dict[df['Dates'].iloc[i]].append(news)
 
     for i in date_dict:
         data_for_df.append((' '.join(date_dict[i]).lower(), i))
@@ -168,7 +170,7 @@ for i in df1['Ticker']:
 def plot():
     plot_number_of_news = []
     for i in range(len(df1['Ticker'])):
-        if df1['Ticker'][i] in ticker:
+        if df1['Ticker'].iloc[i] in ticker:
             number_of_news[df1['Ticker'][i]] += 1
     top_coms = {k: v for k, v in sorted(number_of_news.items(), key=lambda item: item[1])}
     for i, j in top_coms.items():
@@ -189,7 +191,7 @@ def plot():
 def create_Kmanes():
     total_news = []
     for i in range(len(df1['Headlines'])):
-        news = nltk.word_tokenize(df1['Headlines'][i].lower())
+        news = nltk.word_tokenize(df1['Headlines'].iloc[i].lower())
         filtered_sentence = [w for w in news if w not in stop_words]
         stemmed_sentence = [ps.stem(w) for w in filtered_sentence]
         news = ' '.join(stemmed_sentence)
@@ -243,9 +245,10 @@ dist, ind = tree.query(centroids, k=2)
 #             for j in cluster_news[index]:
 #                 print(index, df1['Headlines'][j], df1['Dates'][j])
 
-df = stopwords_and_stemmimg(df1)
+df = stopwords_and_stemmimg(df1, df2)
 traindata = df.iloc[:int(len(df['Headlines'])*0.8)]
 testdata = df.iloc[int(len(df['Headlines'])*0.8):]
+
 
 """Grid Search for Random Forest"""
 # pipeline = Pipeline([
@@ -286,36 +289,85 @@ randomclassifier.fit(traindataset, traindata['Label'])
 testdataset = tfidfconverter.transform(testdata['New_Headlines'])
 predictions = randomclassifier.predict(testdataset)
 predictions_prob = randomclassifier.predict_proba(testdataset)
-
 accuarcy = accuracy_score(testdata['Label'], predictions)
 classification = classification_report(testdata['Label'], predictions)
 matrix = confusion_matrix(testdata['Label'], predictions)
-print(accuarcy)
-print(classification)
-print(matrix)
+# print(accuarcy)
+# print(classification)
+# print(matrix)
 
 """MLP"""
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(units=1024, activation='relu', kernel_initializer='he_uniform'))
-model.add(tf.keras.layers.Dense(units=64, activation='relu'))
-model.add(tf.keras.layers.Dense(units=1, activation='sigmoid',))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(traindataset.toarray(), traindata['Label'], epochs=20, batch_size=32, verbose=1, validation_split=0.2)
-
-val_loss, val_acc = model.evaluate(testdataset.toarray(), testdata['Label'])
-print(val_acc)
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Dense(units=1024, activation='relu', kernel_initializer='he_uniform'))
+# model.add(tf.keras.layers.Dense(units=64, activation='relu'))
+# model.add(tf.keras.layers.Dense(units=1, activation='sigmoid',))
+#
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.fit(traindataset.toarray(), traindata['Label'], epochs=20, batch_size=32, verbose=1, validation_split=0.2)
+#
+# val_loss, val_acc = model.evaluate(testdataset.toarray(), testdata['Label'])
+# print(val_acc)
 # print(np.array(traindata['Label']).reshape(1, -1, 1))
 
 """LSTM"""
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(units=256, input_shape=(1, traindataset.toarray().shape[1]), return_sequences=True))
-model.add(tf.keras.layers.LSTM(units=256, input_shape=(1, traindataset.toarray().shape[1])))
-model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.LSTM(units=256, input_shape=(1, traindataset.toarray().shape[1]), return_sequences=True))
+# model.add(tf.keras.layers.LSTM(units=256, input_shape=(1, traindataset.toarray().shape[1])))
+# model.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+#
+# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.fit(np.array(traindataset.toarray()).reshape(traindataset.toarray().shape[0], 1, traindataset.toarray().shape[1]), traindata['Label'], epochs=10, verbose=1, validation_split=0.3, shuffle=False)
+#
+# val_loss, val_acc = model.evaluate(np.array(testdataset.toarray()).reshape(testdataset.toarray().shape[0], 1, testdataset.toarray().shape[1]), testdata['Label'])
+# print(val_acc)
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(np.array(traindataset.toarray()).reshape(traindataset.toarray().shape[0], 1, traindataset.toarray().shape[1]), traindata['Label'], epochs=10, verbose=1, validation_split=0.3, shuffle=False)
+filename = 'finalized_model.sav'
+pickle.dump(randomclassifier, open(filename, 'wb'))
+pickle.dump(tfidfconverter,open('feature.pkl', 'wb'))
 
-val_loss, val_acc = model.evaluate(np.array(testdataset.toarray()).reshape(testdataset.toarray().shape[0], 1, testdataset.toarray().shape[1]), testdata['Label'])
-print(val_acc)
+def testing_get_news():
+    headlines, dates, tickers, total = [], [], [], []
+    for i in range(1):
+        i += 1
+        url = f"https://seekingalpha.com/market-news/top-news?page={str(i)}"
+        headers = {
+            "content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "https://seekingalpha.com",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+        response = requests.get(url = url, headers = headers)
+        while response.status_code != 200:
+            response = requests.get(url=url, headers=headers)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for script in soup.find_all('div', class_="title"):
+                headlines.append(script.text)
+            for script in soup.find_all('span', class_="item-date"):
+                dates.append(script.text)
+            for script in soup.find_all('div', class_="media-left"):
+                tickers.append(script.text)
+            for i in range(len(headlines)):
+                total.append((headlines[i], dates[i], tickers[i]))
+    return total
 
+def testing(start, end, today, yesterday):
+    news = testing_get_news()
+    ytd_news = [i for i in news]
+    change_dates_tocsv(ytd_news, today, yesterday)
+    df1 = pd.read_csv('Top_News_2.csv')
+    df1.iloc[:, 0].replace(['^a-zA-Z'], ' ', regex=True, inplace=True)
+    df2 = create_price(start, end)
+    print(df2)
+    df = stopwords_and_stemmimg(df1, df2)
+    for i in range(len(df['Label'])):
+        print('Actual: ', df['Label'].iloc[i])
+    tfidfconverter = pickle.load(open('feature.pkl', 'rb'))
+    dataset = tfidfconverter.transform(df['New_Headlines'])
+    loaded_model = pickle.load(open(filename, 'rb'))
+    result = loaded_model.predict(dataset)
+    print('Predict: ', result[0])
+
+start = datetime.datetime(2020, 6, 26)
+end = datetime.datetime(2020, 6, 26)
+today = 'Jul. 8, 2020'
+yesterday = 'Jul. 7, 2020'
+testing(start, end, today, yesterday)
